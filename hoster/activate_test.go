ed29d1d27a2f362e8286a9357dded9a190835a25
Marek Dziewit <marek2901@gmail.com>
2021-10-22 23:26:44 +0200

    Go fmt

--- hoster/activate_test.go
+++ hoster/activate_test.go
@@ -1,16 +1,19 @@
 package hoster_test
 
 import (
-	"github.com/stretchr/testify/assert"
 	"hoster/hoster"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestActivationHandler_Handle(t *testing.T) {
 	subject := hoster.NewActivationHandler()
 
 	t.Run("Smoke test", func(t *testing.T) {
-		output := captureOutput(t, func() { subject.Handle(fakeCliContext(t, []string{"test"})) })
+		output := captureOutput(t, func() {
+			subject.Handle(fakeCliContext(t, []string{"test"}))
+		})
 		assert.Equal(t, "TODO activate test\n", output)
 	})
 }
